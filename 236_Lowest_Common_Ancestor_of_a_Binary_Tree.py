68b05e019c90533ada08b1d007b7dca12e339cdd
yuqingchen <chenyuqing0320@gmail.com>
2020-04-22 16:59:06 -0700

    Add files via upload

---
+++ 236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -0,0 +1,22 @@
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: A: A TreeNode in a Binary.
+    @param: B: A TreeNode in a Binary.
+    @return: Return the least common ancestor(LCA) of the two nodes.
+    """
+    def lowestCommonAncestor(self, root, A, B):
+        # write your code here
+        if root is None :
+            return None 
+        if root == A or root == B :
+            return root 
+        left = self.lowestCommonAncestor(root.left, A, B)
+        right = self.lowestCommonAncestor(root.right, A, B)
+        if left is not None and right is not None :
+            return root
+        if left is not None:
+            return left
+        if right is not None:
+            return right
+        return None
